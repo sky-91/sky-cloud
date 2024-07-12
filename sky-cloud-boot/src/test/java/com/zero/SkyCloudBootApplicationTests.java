13481d29e9d624c801fce683b87ca61589845386
YCKJ4279 <qiuxingjie@cloudwalk.com>
2024-07-12 17:47:26 +0800

    项目模块分类初始化

---
+++ sky-cloud-boot/src/test/java/com/zero/SkyCloudBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyCloudBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
